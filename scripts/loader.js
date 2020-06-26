2ef8d86c6ca21415d0f0e64c7b7e0c3c98719e41
Carlos Zinato <carlosh.lz@hotmail.com>
2020-06-26 21:19:06 +0200

    path

--- scripts/loader.js
+++ scripts/loader.js
@@ -1,14 +1,14 @@
 function preload() {
-    imgBackground = loadImage("~/assets/images/background/forest.png");
+    imgBackground = loadImage("../assets/images/background/forest.png");
     imgHero = loadImage(heroSprite.imagePath);
     imgEnemy = loadImage(enemySprite.imagePath);
     imgEnemyTroll = loadImage(enemyTrollSprite.imagePath);
     imgEnemyFlying = loadImage(enemyFlyingSprite.imagePath);
-    gameOverImage = loadImage("~/assets/images/assets/game-over.png");
-    startupImage = loadImage("~/assets/images/assets/start_bg.png");
-    heartImage = loadImage("~/assets/images/assets/heart.png");
-    startupFont = loadFont("~/assets/images/assets/fontStart.otf");
-    bgMusic = loadSound("~/assets/audio/bg_music.mp3");
+    gameOverImage = loadImage("../assets/images/assets/game-over.png");
+    startupImage = loadImage("../assets/images/assets/start_bg.png");
+    heartImage = loadImage("../assets/images/assets/heart.png");
+    startupFont = loadFont("../assets/images/assets/fontStart.otf");
+    bgMusic = loadSound("../assets/audio/bg_music.mp3");
 
     // Levels
     level = loadJSON("/levels/01.json");
